5e63d764e775913395752b1d11b8a83efeb7972a
Seyed (Yahya) Shirazi <shirazi@ieee.org>
2026-01-26 06:55:11 -0800

    Bump version to 0.5.1.dev0 (#83)

--- src/version.py
+++ src/version.py
@@ -1,7 +1,7 @@
 """Version information for OSA."""
 
-__version__ = "0.5.0"
-__version_info__ = (0, 5, 0)
+__version__ = "0.5.1.dev0"
+__version_info__ = (0, 5, 1, "dev")
 
 
 def get_version() -> str:
